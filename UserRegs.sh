52e31efe03238ca295820877af2f885d8be311a1
madhuvayya <madhu.vayya@gmail.com>
2020-04-25 11:06:54 +0530

    Added function to validate mobile number

--- UserRegs.sh
+++ UserRegs.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
 NAME_PATTERN="^[[:upper:]][[:lower:]]{2,}$"
-EMAIL_PATTERN="^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+EMAIL_PATTERN="^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+MOB_NUM_PATTERN="^[1-9]{2}[ ]*[6-9]{1}[0-9]{9}$"
 
 patternValidator(){
 	if [[ $1 =~ $2 ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+
+moblieNumValidator(){
+	if [[ $mobileNum =~ $MOB_NUM_PATTERN ]]
 	then
 		echo Valid
 	else
@@ -26,3 +36,8 @@ echo "enter email"
 read email
 
 patternValidator $email $EMAIL_PATTERN
+
+echo "enter mobile number"
+read mobileNum
+
+moblieNumValidator
