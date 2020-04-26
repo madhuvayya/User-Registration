a415643a2c9540d854ef0149dc19728bf446e3d8
madhuvayya <madhu.vayya@gmail.com>
2020-04-26 19:01:24 +0530

    Added password regex to validate atleast one upper case

--- UserRegs.sh
+++ UserRegs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 
-NAME_PATTERN="^[[:upper:]][[:lower:]]{2,}$"
-EMAIL_PATTERN="^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
-MOB_NUM_PATTERN="^[1-9]{2}[ ]*[6-9]{1}[0-9]{9}$"
-PSWD_PATTERN="[0-9a-zA-Z]{8}"
+NAME_PAT="^[[:upper:]][[:lower:]]{2,}$"
+
+EMAIL_PAT="^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+
+MOB_NUM_PAT="^[1-9]{2}[ ]*[6-9]{1}[0-9]{9}$"
+
+PSWD_PAT=".{8,}"
+PSWD_PAT="[[:upper:]]+"
 
 patternValidator(){
 	if [[ $1 =~ $2 ]]
@@ -15,7 +19,7 @@ patternValidator(){
 }
 
 moblieNumValidator(){
-	if [[ $mobileNum =~ $MOB_NUM_PATTERN ]]
+	if [[ $1 =~ $MOB_NUM_PAT ]]
 	then
 		echo Valid
 	else
@@ -26,24 +30,24 @@ moblieNumValidator(){
 echo "enter first name"
 read fName
 
-patternValidator $fName $NAME_PATTERN
+patternValidator $fName $NAME_PAT
 
 echo "enter last name"
 read lName
 
-patternValidator $lName $NAME_PATTERN
+patternValidator $lName $NAME_PAT
 
 echo "enter email"
 read email
 
-patternValidator $email $EMAIL_PATTERN
+patternValidator $email $EMAIL_PAT
 
 echo "enter mobile number"
 read mobileNum
 
-moblieNumValidator
+moblieNumValidator $mobileNum
 
 echo "enter password"
 read pswd
 
-patternValidator $pswd $PSWD_PATTERN
+patternValidator $pswd $PSWD_PAT
